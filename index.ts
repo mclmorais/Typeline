6176d44a85c1c0b77fa81973bdf91012f38a3d50
Marcelo Fernandes <marcelodotmorais@gmail.com>
2026-02-15 14:40:31 -0300

    scaffolding

--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-console.log("Hello via Bun!");
+import { AsyncLocalStorage } from 'node:async_hooks'
+
+const asyncLocalStorage = new AsyncLocalStorage<string>()
+
+
+type Step = {
+    name: string,
+    run: () => void
+}
+
+const steps: Step[] = []
+
+function runPipeline(steps: Step[]) {
+    for (const step of steps) {
+        console.log(`Running step: ${step.name}`)
+        step.run()
+    }
+}
+
+const SetValueStep: Step = {
+    name: "Set Value Step",
+    run: () => {
+        asyncLocalStorage.run("value", () => {
+            console.log("Value set")
+        })
+    }
+}
+
+const GetValueStep: Step = {
+    name: "Get Value Step",
+    run: () => {
+        const value = asyncLocalStorage.getStore()
+        console.log("Value: ", value)
+    }
+}
+
+runPipeline([SetValueStep, GetValueStep])
